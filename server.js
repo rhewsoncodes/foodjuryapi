7700204ad10372a24ca19f89543fc568d76332d6
robert hewson <rhewsoncodes@gmail.com>
2022-11-30 13:50:49 -0500

    Added logout functionality

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use(cookieParser());
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
 app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
 // error handler
 app.use(errorHandler);
